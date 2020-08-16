279a2e69ef8f2172aad4bbc99b81b9e65c17f0b3
DmitriyAlyashev <dmitriy.alyashev@yandex.ru>
2020-08-16 16:57:38 +0300

    Провел рефакторинг, вынесли логику ввода логина и пароля, и создания наименования групп в параметры метода. Исправил багу.

--- test_add_group.py
+++ test_add_group.py
@@ -12,46 +12,55 @@ class TestAddGroup(unittest.TestCase):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
-    def test_add_group(self):
-        wd = self.wd
-        self.create_group(wd)
-        self.return_group_page(wd)
-        self.logout(wd)
-
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def return_group_page(self, wd):
-        wd.find_element_by_link_text("group page").click()
-
-    def create_group(self, wd):
-        # open home page
+    def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
-        self.login(wd)
+
+    def login(self, wd, user_name, password):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("%s" % user_name)
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_groups_page(self, wd):
+        wd.find_element_by_link_text("groups").click()
+
+    def create_group(self, wd, name, header, footer):
         self.open_groups_page(wd)
         # fill group form
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("test")
+        wd.find_element_by_name("group_name").send_keys("%s" % name)
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("test")
+        wd.find_element_by_name("group_header").send_keys("%s" % header)
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("test")
+        wd.find_element_by_name("group_footer").send_keys("%s" % footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
 
-    def open_groups_page(self, wd):
-        # open groups page
-        wd.find_element_by_link_text("groups").click()
+    def return_group_page(self, wd):
+        wd.find_element_by_link_text("group page").click()
 
-    def login(self, wd):
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def test_add_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, user_name="admin", password="secret")
+        self.create_group(wd, name="test", header="test", footer="test")
+        self.return_group_page(wd)
+        self.logout(wd)
+
+    def test_add_emty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, user_name="admin", password="secret")
+        self.create_group(wd, name="", header="", footer="")
+        self.return_group_page(wd)
+        self.logout(wd)
 
     def is_element_present(self, how, what):
         try:
