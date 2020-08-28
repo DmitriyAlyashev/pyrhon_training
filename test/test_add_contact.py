13dcbb7389d020de76c773c1ff8d57e22e6ee1b1
DmitriyAlyashev <dmitriy.alyashev@yandex.ru>
2020-08-28 09:37:33 +0300

    Доработал ДЗ - 3. Перевел проект на pytest и реализовал fixture для контактов. Создал классы-помощники и перенес вспомогательные функции в эти классы Создал класс, который инициализирует всех помощников

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,28 +1,22 @@
 import pytest
+from fixture.application import Application
 from model.contact import Contact
-from fixture.applicationcontact import ApplicationContact
 
 
 @pytest.fixture
 def app(request):
-    fixture = ApplicationContact()
+    fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
 
 
 def test_add_contact(app):
-    app.open_home_page()
-    app.login(user_login="admin", user_password="secret")
-    app.open_contact_page()
-    app.create_contact(Contact(firstname="Test", middlename="Testovich", lastname="Testovovic", nickname="TeSt123", title="Test", company="Tsts", address="Test52", home="87346345",
-                            mobile="2342523523", work="2342354235", fax="557567", email="yugwe@eiwtf.ru", email2="orbf@ut.com", email3="reouih@af.com",
-                            homepage="www.facebook.com", bday="1", bmonth="April", byear="1222", aday="2", amonth="November", ayear="1990", address2="efwfefwefe34",
-                            phone2="sdfdsf12", notes="efewfefefrrevre23jpji ojopeihcop4"))
+    app.session.login(username="admin", password="secret")
+    app.contact.create(Contact(contact_firstname="Peter", contact_lastname="Shteiman", contact_address="Russia"))
+    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.open_home_page()
-    app.login(user_login="admin", user_password="secret")
-    app.open_contact_page()
-    app.create_contact(Contact(firstname="", middlename="", lastname="", nickname="", title="", company="", address="", home="", mobile="", work="", fax="", email="", email2="", email3="",
-                            homepage="", bday="1", bmonth="April", byear="1222", aday="2", amonth="November", ayear="1990", address2="", phone2="", notes=""))
+    app.session.login(username="admin", password="secret")
+    app.contact.create(Contact(contact_firstname="", contact_lastname="", contact_address=""))
+    app.session.logout()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -11,12 +11,12 @@ def app(request):
 
 
 def test_add_group(app):
-    app.session.login(user_name="admin", password="secret")
-    app.group.create(Group(name="test", header="test", footer="test"))
+    app.session.login(username="admin", password="secret")
+    app.group.create(Group(name="new group", header="etc", footer="other"))
     app.session.logout()
 
 
-def test_add_emty_group(app):
-    app.session.login(user_name="admin", password="secret")
+def test_add_empty_group(app):
+    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
