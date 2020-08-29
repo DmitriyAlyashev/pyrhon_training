8474fcd492a9d81b660aebd4949e02787fd0cfd3
DmitriyAlyashev <dmitriy.alyashev@yandex.ru>
2020-08-29 23:09:48 +0300

    Сделал fixture обшую для всех тестов.  Добавил параметр (scope = "session") для ускорения прохождения тестов.

--- fixture/group.py
+++ fixture/group.py
@@ -29,24 +29,10 @@ class GroupHelper:
     def delete_first_group(self):
         wd = self.app.wd
         self.open_group_page()
-        # select first group
+        #select first group
         wd.find_element_by_name("selected[]").click()
-        # submit deletion
+        #submit deletion
         wd.find_element_by_name("delete").click()
-        self.return_to_groups_page()
-
-    def edit_first_group(self):
-        wd = self.app.wd
-        self.open_group_page()
-        # select first group
-        wd.find_element_by_name("selected[]").click()
-        # making edition
-        wd.find_element_by_name("edit").click()
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("edited")
-        # submit group updating
-        wd.find_element_by_name("update").click()
         self.return_to_groups_page()
 
     def return_to_groups_page(self):
