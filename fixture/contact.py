3896bb16e41a6bc5491dc1f2ca389979e115ec40
DmitriyAlyashev <dmitriy.alyashev@yandex.ru>
2020-08-30 07:40:43 +0300

    Добавил тест изменения Контакта.

--- fixture/contact.py
+++ fixture/contact.py
@@ -33,6 +33,18 @@ class ContactHelper:
         # accept and close popup
         wd.switch_to_alert().accept()
 
+    def edit_first_contact(self):
+        wd = self.app.wd
+        # select edition for the first contact
+        wd.find_element_by_xpath("/html/body/div/div[4]/form[2]/table/tbody/tr[2]/td[8]/a/img").click()
+        # edit contact info
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys("Edited")
+        # submit contact updating
+        wd.find_element_by_name("update").click()
+        self.return_to_home_page()
+
     def return_to_home_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("home page").click()
